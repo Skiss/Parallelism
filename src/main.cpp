c1ac250c711df75ff186c341d6fb38998bf6f054
Jeremy <jeremy.chanut@gmail.com>
2012-07-14 02:40:25 +0200

    Adding chunk of frame.

--- src/main.cpp
+++ src/main.cpp
@@ -7,24 +7,24 @@ int main( int argc, char** argv )
 {
     tbb::task_scheduler_init init;
 
-    Mat image;
-    image = imread( argv[1], 1 );
-
-    if( argc != 2 || !image.data )
+    if (argc != 2)
     {
-        printf( "No image data \n" );
+        std::cerr << "More arguments please." << std::endl;
         return -1;
     }
 
-    tbb::tick_count  t0 = tbb::tick_count::now();
-    Mat res = proc::blur(image);
-    tbb::tick_count  t1 = tbb::tick_count::now();
-    std::cout << "Sequential : " << (t1 - t0).seconds() << "s" << std::endl;
+    // tbb::tick_count  t0 = tbb::tick_count::now();
+    // Mat res = proc::blur(image);
+    // tbb::tick_count  t1 = tbb::tick_count::now();
+    // std::cout << "Sequential : " << (t1 - t0).seconds() << "s" << std::endl;
 
-    tbb::tick_count  t2 = tbb::tick_count::now();
-    res = proc::blur(image, true);
-    tbb::tick_count  t3 = tbb::tick_count::now();
-    std::cout << "Parallel : " << (t3 - t2).seconds() << "s" << std::endl;
+    // tbb::tick_count  t2 = tbb::tick_count::now();
+    // res = proc::blur(image, true);
+    // tbb::tick_count  t3 = tbb::tick_count::now();
+    // std::cout << "Parallel : " << (t3 - t2).seconds() << "s" << std::endl;
+
+    cv::VideoCapture vid(argv[1]);
+    std::cout << vid.get(CV_CAP_PROP_FRAME_COUNT) << std::endl;
 
     // namedWindow( "Display Image", CV_WINDOW_AUTOSIZE );
     // imshow( "Blur", res);
