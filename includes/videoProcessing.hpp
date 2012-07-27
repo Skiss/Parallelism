c0b3f251163f745488fa7583a5be8768038fc9ef
bara3r <bara3r+github@gmail.com>
2012-07-27 14:58:40 +0200

    Add blend.

--- includes/videoProcessing.hpp
+++ includes/videoProcessing.hpp
@@ -3,13 +3,18 @@
 
 #include <cv.h>
 
+#include <tbb/task_scheduler_init.h>
+#include <tbb/blocked_range.h>
+#include <tbb/blocked_range2d.h>
+#include <tbb/parallel_for.h>
+#include <tbb/tick_count.h>
 
 namespace video
 {
     bool isSwapable(const cv::Mat& img);
-    cv::Mat swapContent(cv::Mat& img1, const cv::Mat& img2);
+    cv::Mat swapContent(cv::Mat& img1, const cv::Mat& img2, bool para);
 
-    cv::Mat blend(const cv::Mat& img1, const cv::Mat& img2);
+    cv::Mat blend(cv::Mat& img1, const cv::Mat& img2, bool para);
 }
 
 #endif /* _VIDEOPROCESSING_H_ */
--- src/videoProcessing.cpp
+++ src/videoProcessing.cpp
@@ -8,12 +8,25 @@ namespace video
         return (pix[1] >= 160 && pix[0] < 50 && pix[2] < 110);
     }
 
-    cv::Mat swapContent(cv::Mat& img1, const cv::Mat& img2)
+    cv::Mat swapContent(cv::Mat& img1, const cv::Mat& img2, bool para)
     {
-        for (int i = 0; i < img1.rows; ++i) {
-            for (int j = 0; j < img1.cols; ++j) {
-                if (isSwapable(img1.at<cv::Vec3b>(i, j))) {
-                    img1.at<cv::Vec3b>(i, j) = img2.at<cv::Vec3b>(i, j);
+        if (para) {
+            tbb::blocked_range2d<unsigned, unsigned> range(0, img1.rows, 0, img1.cols);
+            tbb::parallel_for(range, [=, &img1](const tbb::blocked_range2d<unsigned, unsigned>& r) {
+                for (unsigned i = r.rows().begin(); i < r.rows().end(); ++i) {
+                    for (unsigned j = r.cols().begin(); j < r.cols().end(); ++j) {
+                        if (isSwapable(img1.at<cv::Vec3b>(i, j))) {
+                            img1.at<cv::Vec3b>(i, j) = img2.at<cv::Vec3b>(i, j);
+                        }
+                    }
+                }
+            });
+        } else {
+            for (int i = 0; i < img1.rows; ++i) {
+                for (int j = 0; j < img1.cols; ++j) {
+                    if (isSwapable(img1.at<cv::Vec3b>(i, j))) {
+                        img1.at<cv::Vec3b>(i, j) = img2.at<cv::Vec3b>(i, j);
+                    }
                 }
             }
         }
@@ -21,12 +34,29 @@ namespace video
         return img1;
     }
 
-    cv::Mat blend(const cv::Mat& img1, const cv::Mat& img2)
+    cv::Mat blend(cv::Mat& img1, const cv::Mat& img2, bool para)
     {
-        cv::Mat res = img1;
+        if (para) {
+            tbb::blocked_range2d<unsigned, unsigned> range(0, img1.rows, 0, img1.cols);
+            tbb::parallel_for(range, [=, &img1](const tbb::blocked_range2d<unsigned, unsigned>& r) {
+                for (unsigned i = r.rows().begin(); i < r.rows().end(); ++i) {
+                    for (unsigned j = r.cols().begin(); j < r.cols().end(); ++j) {
+                        for (int c = 0; c < 3; ++c) {
+                            img1.at<cv::Vec3b>(i, j)[c] = (img1.at<cv::Vec3b>(i, j)[c] + img2.at<cv::Vec3b>(i, j)[c])/2;
+                        }
+                    }
+                }
+            });
+        } else {
+            for (int i = 0; i < img1.rows; ++i) {
+                for (int j = 0; j < img1.cols; ++j) {
+                    for (int c = 0; c < 3; ++c) {
+                        img1.at<cv::Vec3b>(i, j)[c] = (img1.at<cv::Vec3b>(i, j)[c] + img2.at<cv::Vec3b>(i, j)[c])/2;
+                    }
+                }
+            }
+        }
 
-        addWeighted(img1, 0.5, img2, 0.5, 0.0, res);
-
-        return res;
+        return img1;
     }
 }
