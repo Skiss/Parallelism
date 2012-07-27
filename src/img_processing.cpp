4714f9f5377c7a58ddba9b0a0353505936efc596
bara3r <bara3r+github@gmail.com>
2012-07-27 14:25:03 +0200

    Re-indent parallel for.

--- src/img_processing.cpp
+++ src/img_processing.cpp
@@ -1,6 +1,5 @@
 #include "img_processing.hpp"
 
-
 namespace proc
 {
     void blur(cv::Mat& img, bool para)
@@ -74,25 +73,23 @@ namespace proc
         cv::Mat res = cv::Mat::zeros(img.rows, img.cols, img.type());
 
         tbb::blocked_range2d<unsigned, unsigned> range(1, img.rows - 1, 1, img.cols - 1);
-        tbb::parallel_for(range,
-                          [=, &res](const tbb::blocked_range2d<unsigned, unsigned>& r)
-                          {
-                              for (unsigned i = r.rows().begin(); i < r.rows().end(); ++i) {
-                                  for (unsigned j = r.cols().begin(); j < r.cols().end(); ++j) {
-                                      for (int c = 0; c < 3; ++c) {
-                                          int tmp = 0;
-                                          for (int a = 0; a < filter.rows; ++a) {
-                                              for (int b = 0; b < filter.cols; ++b) {
-                                                  tmp += filter.at<double>(a, b) * img.at<cv::Vec3b>(i - 1 + a, j - 1 + b)[c];
-                                              }
-                                          }
-                                          if (tmp > 255) tmp = 255;
-                                          if (tmp < 0) tmp = 0;
-                                          res.at<cv::Vec3b>(i, j)[c] = tmp;
-                                      }
-                                  }
-                              }
-                          });
+        tbb::parallel_for(range, [=, &res](const tbb::blocked_range2d<unsigned, unsigned>& r) {
+            for (unsigned i = r.rows().begin(); i < r.rows().end(); ++i) {
+                for (unsigned j = r.cols().begin(); j < r.cols().end(); ++j) {
+                    for (int c = 0; c < 3; ++c) {
+                        int tmp = 0;
+                        for (int a = 0; a < filter.rows; ++a) {
+                            for (int b = 0; b < filter.cols; ++b) {
+                                tmp += filter.at<double>(a, b) * img.at<cv::Vec3b>(i - 1 + a, j - 1 + b)[c];
+                            }
+                        }
+                        if (tmp > 255) tmp = 255;
+                        if (tmp < 0) tmp = 0;
+                        res.at<cv::Vec3b>(i, j)[c] = tmp;
+                    }
+                }
+            }
+        });
         res.copyTo(img);
     }
 
@@ -125,15 +122,13 @@ namespace proc
         if (para) {
             tbb::blocked_range2d<unsigned, unsigned> range(1, img.rows - 1, 1, img.cols - 1);
             cv::Vec3b inv(255, 255, 255);
-            tbb::parallel_for(range,
-                          [=, &res](const tbb::blocked_range2d<unsigned, unsigned>& r)
-                          {
-                              for (unsigned i = r.rows().begin(); i < r.rows().end(); ++i) {
-                                  for (unsigned j = r.cols().begin(); j < r.cols().end(); ++j) {
-                                          res.at<cv::Vec3b>(i, j) = inv - img.at<cv::Vec3b>(i, j);
-                                  }
-                              }
-                          });
+            tbb::parallel_for(range, [=, &res](const tbb::blocked_range2d<unsigned, unsigned>& r) {
+                for (unsigned i = r.rows().begin(); i < r.rows().end(); ++i) {
+                    for (unsigned j = r.cols().begin(); j < r.cols().end(); ++j) {
+                            res.at<cv::Vec3b>(i, j) = inv - img.at<cv::Vec3b>(i, j);
+                    }
+                }
+            });
         } else {
             for (int i = 1; i < img.rows - 1; ++i) {
                 for (int j = 1; j < img.cols - 1; ++j) {
@@ -152,15 +147,13 @@ namespace proc
 
         if (para) {
             tbb::blocked_range2d<unsigned, unsigned> range(0, img.rows, 0, img.cols);
-            tbb::parallel_for(range,
-                          [=, &res](const tbb::blocked_range2d<unsigned, unsigned>& r)
-                          {
-                              for (unsigned i = r.rows().begin(); i < r.rows().end(); ++i) {
-                                  for (unsigned j = r.cols().begin(); j < r.cols().end(); ++j) {
-                                      res.at<cv::Vec3b>(i, j) = img.at<cv::Vec3b>(i, img.cols - j);
-                                  }
-                              }
-                          });
+            tbb::parallel_for(range, [=, &res](const tbb::blocked_range2d<unsigned, unsigned>& r) {
+                for (unsigned i = r.rows().begin(); i < r.rows().end(); ++i) {
+                    for (unsigned j = r.cols().begin(); j < r.cols().end(); ++j) {
+                        res.at<cv::Vec3b>(i, j) = img.at<cv::Vec3b>(i, img.cols - j);
+                    }
+                }
+            });
         } else {
             for (int i = 0; i < img.rows; ++i) {
                 for (int j = 0; j < img.cols; ++j) {
